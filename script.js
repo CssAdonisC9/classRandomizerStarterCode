c72104b81be1bff55d7b501ff0f5d42011f34798
Adonis Mateo <amatt2000@hotmail.com>
2017-05-04 00:59:04 +0000

    the randomizers now work as intended

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-$(document).ready(function(){
+var students = ["Leslie", "Monserrat", "Adonis"];
 
+$("#studentButton").click(function() {
+  var random = Math.floor(Math.random() * students.length);
+  $("#studentDisplay").append(students[random]);
+});
 
+var teachers = ["Zach", "Aaron", "Julia", "Justin", "Alyxe", "Ms. Williams"];
 
+$("#teacherButton").click(function() {
+  var random = Math.floor(Math.random() * teachers.length);
+  $("#teacherDisplay").append(teachers[random]);
 });
